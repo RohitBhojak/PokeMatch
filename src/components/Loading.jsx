cffb1b5056129bdd99889a904124cee0ddecfb87
Rohit Bhojak <rohitbhojak99@gmail.com>
2026-01-20 22:00:14 +0530

    Create loading screen

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import loadingGif from "../assets/loading.gif";
+import "../styles/loading.css";
+
+export default function Loading() {
+  return (
+    <>
+      <img src={loadingGif} height={100} width={100} alt="Loading" className="loading-bike" />
+      <span className="loading-text">Loading</span>
+    </>
+  );
+}
