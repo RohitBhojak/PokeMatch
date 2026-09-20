2b4e3dcf8b4fd51d8f9c65d19e23e48d0bc39e08
Rohit Bhojak <rohitbhojak99@gmail.com>
2026-09-20 18:13:22 +0530

    Add loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,17 +1,5 @@
-import loadingGif from "../assets/loading.gif";
 import "../styles/loading.css";
 
 export default function Loading() {
-  return (
-    <>
-      <img
-        src={loadingGif}
-        height={100}
-        width={100}
-        alt="Loading"
-        className="loading-bike"
-      />
-      <span className="loading-text">Loading</span>
-    </>
-  );
+  return <span className="loading">Loading</span>;
 }
